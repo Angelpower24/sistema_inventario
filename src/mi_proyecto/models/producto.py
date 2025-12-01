de7cb1670afec64d75f4965af3445e3f8b23e6ab
Angelpower24 <angel.floress@istpargentina.edu.pe>
2025-11-30 23:20:09 -0500

    commit 3: continuando con el avance

--- src/mi_proyecto/models/producto.py
+++ src/mi_proyecto/models/producto.py
@@ -12,3 +12,19 @@ class Categoria(Enum):
     ROPA = "Ropa"
     LIBROS = "Libros"
     OTROS = "Otros"
+
+class Producto:
+    """
+    Representa un producto en el inventario.
+
+    Attributes:
+        id_producto (int): Identificador unico
+        nombre (str): Nombre del producto
+        descripcion (str): Descripcion del producto
+        precio (float): Precio unitario
+        cantidad (int): Cantidad en stock
+        categoria (Categoria): Categoria del producto
+        fecha_creacion (str): Fecha de creacion
+    """
+    
+    __contador = 1000
